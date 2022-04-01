2f4b5f2f141044bfcb235067f0ee246bc04694c9
Cameron <sicklecj@gmail.com>
2022-04-01 15:00:06 -0500

    add script.js file and adjust html

---
+++ Develop/script.js
@@ -0,0 +1,8 @@
+// var hoursList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+// $('.hour').each(function () {
+//     $(this).text(hoursList)
+// })
+
+var timeEl = $("#time")
+var timeMethod = moment().format('MMMM Do YYYY, h:mm:ss a');
