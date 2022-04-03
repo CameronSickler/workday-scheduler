59d5942a3856c310097aee8714bce41012075b50
Cameron <sicklecj@gmail.com>
2022-04-03 16:04:36 -0500

    add psuedo code to JS

--- Develop/script.js
+++ Develop/script.js
@@ -18,3 +18,12 @@
 // moment.js time function and id for HTML
 var time = moment().format("MMM Do YY");
 $("#currentDay").html(time);
+
+// add event listener code using JQuery for saveBtn click
+
+//save local storage saves. Need to create vars that save data to send to local storage.
+
+//Need a function loop? that checks the timeblock IDs for time and changes styles accordingly. Idk
+//if I need this all as a single function or maybe an if else loop through?
+
+//need to get all information from local storage on refresh/page load
