85e9129a6d242c9af7289102704f42a60e05b2e7
Cameron <sicklecj@gmail.com>
2022-04-03 18:30:29 -0500

    add timeMonitor functionality with if else statements

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,11 @@ var momentTimeDate = moment().format("MMM Do YY");
 $("#currentDay").html(momentTimeDate);
 
 // add event listener code using JQuery for saveBtn click
-$(document).on("click", function () {
-    //saves the current element description as var task
+$(".saveBtn").on("click", function () {
+    //saves the textarea value as var task on save btn click
     var task = $(this).siblings(".description").val();
     console.log(task);
+    //saves the id as the time on save btn click
     var time = $(this).parent().attr("id");
     console.log(time);
 
@@ -19,10 +20,34 @@ $(document).on("click", function () {
 //Need a function loop? that checks the timeblock IDs for time and changes styles accordingly. Idk
 
 function timeMonitor() {
-    console.log(time);
+
+    var setMonitorTimeNow = moment().hour();
+
     // i need to check the moment.js time when this runs and then loop over the timeblocks to 
     //update their styles depending on what time it is.
+
+    $(".time-block").each(function () {
+        // save the id attributes returned as an array of values to a variable
+        var timeBlockChecker = ($(this).attr("id").split("hour")[1]);
+        console.log(timeBlockChecker);
+        console.log(setMonitorTimeNow);
+
+        if (setMonitorTimeNow < timeBlockChecker) {
+            console.log("There is still time to finish this task");
+            $(this).addClass("future");
+        } else if (setMonitorTimeNow == timeBlockChecker) {
+            console.log("This task must be completed now");
+            $(this).addClass("present");
+        } else {
+            console.log("This task is overdue today");
+            $(this).addClass("past");
+        }
+        console.log("===");
+    })
 }
 
 
+$("#hour9.description").val(localStorage.getItem("hour9"));
 //need to get all information from local storage on refresh/page load
+
+timeMonitor();
