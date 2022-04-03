2bf5a61c840af85a021bdfec903aec75088f2876
Cameron <sicklecj@gmail.com>
2022-04-03 16:16:07 -0500

    add local storage function for now and add timeMonitor function with refined psuedo code

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,15 @@ $(document).on("click", function () {
 
 //save local storage saves. Need to create vars that save data to send to local storage.
 
+localStorage.setItem(task, time);
+
 //Need a function loop? that checks the timeblock IDs for time and changes styles accordingly. Idk
-//if I need this all as a single function or maybe an if else loop through?
+
+function timeMonitor() {
+    console.log(time);
+    // i need to check the moment.js time when this runs and then loop over the timeblocks to 
+    //update their styles depending on what time it is.
+}
+
 
 //need to get all information from local storage on refresh/page load
